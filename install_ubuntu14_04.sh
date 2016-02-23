9af7c48fe85ffd8bdae2ea57dc633ba4bc680550
prokowski <peter.rokowski@lmco.com>
2016-02-23 18:12:58 -0500

    adding ubuntu install script

---
+++ install_ubuntu14_04.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+sudo apt-get update;
+sudo apt-get -y upgrade;
+
+# add all apt-repository's
+# tmux
+sudo -E add-apt-repository -y ppa:pi-rho/dev;
+# numix icons
+sudo -E add-apt-repository -y ppa:numix/ppa;
+# cinnamon DE
+sudo -E add-apt-repository -y ppa:tsvetko.tsvetkov/cinnamon;
+# plank
+sudo -E add-apt-repository -y ppa:ricotz/docky;
+
+
+# update
+sudo apt-get update;
+
+# install packages
+sudo apt-get -y install synaptic gdebi;
+sudo apt-get -y install vim git htop tmux;
+
+# install numix theme/icons
+sudo apt-get -y install numix-icon-theme-circle numix-gtk-theme;
+
+# install cinnamon
+sudo apt-get -y install cinnamon;
+
+# install planky
+sudo apt-get -y install plank;
+
+# install ctags for vim
+sudo apt-get -y install exuberant-ctags;
+
+
+sudo apt-get autoclean;
+
+# install system utilities
+# systemback
+# gparted
+# Dropbox
+#cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
