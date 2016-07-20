4ed390db088c0e38b23ee50fe7a0e99100a5d437
Prokow01 <peterrokowski@gmail.com>
2016-07-19 22:06:53 -0400

    adding support for cask

--- install/brew.sh
+++ install/brew.sh
@@ -6,4 +6,13 @@ brew install git;
 brew install tmux;
 
 brew install tree;
+
+brew install cask;
+
+# cask programs
+brew cask install spotify;
+
+brew cask install spectacle;
+# add config script
+
 exit 0
