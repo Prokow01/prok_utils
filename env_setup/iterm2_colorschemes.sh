f109a94a7a851a1d00d479aab259640b95db37e3
Peter <peter.rokowski@lmco.com>
2017-05-04 18:12:50 -0400

    adding in colorscheme support for iterm2

---
+++ env_setup/iterm2_colorschemes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "downloading iTerm 2 Colorschemes";
+
+CONFIG_DIR="$HOME/.config";
+
+if [ ! -d "$CONFIG_DIR" ]; then
+    mkdir $CONFIG_DIR;
+    echo "making config dir at $CONFIG_DIR";
+fi
+
+git clone https://github.com/mbadolato/iTerm2-Color-Schemes.git $CONFIG_DIR;
+
+echo "done"
