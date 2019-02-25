24f36847adb1ac65a5f91df7d54b7fbcbe96d4ee
prokowski <peter.rokowski@lmco.com>
2019-02-25 09:56:23 -0500

    adding in anaconda3 path, ignoring bash_aliases; adding to toggle-proxy script

--- toggle-proxy.sh
+++ toggle-proxy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# check if any proxy is specified; exit with bad input error code
+if [ -z "${1}" ]; then
+	 echo 'empty input';
+	exit 128; 
+fi
+
 PROXY=$1;
 HTTP="http://"${PROXY};
 HTTPS="https://"${PROXY};
-
-# Git Proxies
-git config --global http.proxy ${HTTP};
-git config --global https.proxy ${HTTPS};
 
 # Change .bashrc proxies
 SED_HTTP="s/^\(export http_proxy=\).*/\1http:\/\/${PROXY}/";
@@ -17,9 +19,47 @@ sed -i '' "${SED_HTTPS}" .bashrc;
 source .bashrc;
 
 
+# Git Proxies
+echo 'Checking for Git';
+which git;
+
+if [ $? -eq 0 ]; then
+	echo 'Found Git --- Changin Proxy';
+	# Unset old proxy
+	git config --global --unset http.proxy;
+	git config --global --unset https.proxy;
+
+	# Set the new one
+	git config --global http.proxy ${HTTP};
+	git config --global https.proxy ${HTTPS};
+	echo 'done';
+else
+	echo 'Could not find Git --- Skipping';
+fi
+
 
 # Other proxies
 # - git-svn (~/.subversion/servers)
 # - maven (~/.m2/settings.xml)
-# - node (npm config set http-proxy)
+# [x] node (npm config set http-proxy)
+
+# could abstract all of this checking and then executing to a function...
+
+# Node 
+echo 'Checking for NPM';
+which npm;
+
+if [ $? -eq 0 ]; then
+	echo 'Found npm --- Changin Proxy';
+	# Unset old proxy
+	npm config unset proxy;
+	npm config unset https-proxy;
+
+	# Set new one
+	npm config set proxy $HTTP;
+	npm config set https-proxy $HTTPS;
+	echo 'done';
+else
+	echo 'Could not find npm --- Skipping';
+fi
 
